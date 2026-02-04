8fc776b6f95488dbde39c247004cb959ee755c38
Lemsito22 <lemquilca22@gmail.com>
2026-02-04 10:55:55 +0100

    .

--- java/src/banco.java
+++ java/src/banco.java
@@ -10,7 +10,7 @@ class Persona {
         this.nombre = nombre;
         this.dni = dni;
         this.edad = edad;
-        this.dinero = 50;
+        this.dinero = 0;
     }
 
     //METODOS QUE SE USAN DENTRO DEL OBJETO
@@ -19,13 +19,26 @@ class Persona {
     }
     public void meterdinero(int ingresardinero){
         dinero+=ingresardinero;
-        System.out.println("Saldo actual: "+dinero);
+        System.out.println("Saldo actual: "+dinero+"€");
+    }
+    public void sacardinero(int sacardinero){
+        if (dinero<sacardinero){
+            System.out.println("Saldo insuficiente");
+            
+        } else {
+            dinero-=sacardinero;
+            System.out.println("Saldo actual: "+dinero+"€");
+        }
+
+    }
+    public void saldoactual(){
+        System.out.println("Nombre: "+nombre+"\nDNI:"+dni+"\nEdad: "+edad+"\nDinero actual: "+dinero+"€");
     }
 }
 
 public class banco {
     public static void quehacer(){
-        System.out.println("¿Que quieres hacer hoy?\n1. Ingresar dinero\n2. Retirar dinero\n3. Ver saldo");
+        System.out.println("¿Que quieres hacer hoy?\n1. Ingresar dinero\n2. Retirar dinero\n3. Ver información de cuenta");
     }
 
     static void main(String[] args) {
@@ -34,9 +47,51 @@ public class banco {
         Persona segundapersona = new Persona("Kinberly",40488420,18);
 
         mipersona.saludo();
-        quehacer();
+        boolean salir=false;
 
-        int ingresardinero=sc.nextInt();
-        mipersona.meterdinero(ingresardinero);
+        while (!salir){
+            int respuesta=0;
+            quehacer();
+            int quehacer= sc.nextInt();
+            switch (quehacer){
+                case 1:
+                    System.out.println("¿Cúanto dinero deseas ingresar?");
+                    int ingresardinero=sc.nextInt();
+                    mipersona.meterdinero(ingresardinero);
+                    System.out.println("¿Quieres realizar otra operación?\n1.Si\n2.No");
+                    respuesta=sc.nextInt();
+                    if (respuesta!=2){
+                        continue;
+                    } else {
+                        System.out.println("Tenga un buen día");
+                        return;
+                    }
+                case 2:
+                    System.out.println("¿Cúanto retirarás?");
+                    int sacardinero=sc.nextInt();
+                    mipersona.sacardinero(sacardinero);
+                    System.out.println("¿Quieres realizar otra operación?\n1.Si\n2.No");
+                    respuesta=sc.nextInt();
+                    if (respuesta!=2){
+                        continue;
+                    } else {
+                        System.out.println("Tenga un buen día");
+                        return;
+                    }
+                case 3:
+                    mipersona.saldoactual();
+                    System.out.println("¿Quieres realizar otra operación?\n1.Si\n2.No");
+                    respuesta=sc.nextInt();
+                    if (respuesta!=2){
+                        continue;
+                    } else {
+                        System.out.println("Tenga un buen día");
+                        return;
+                    }
+            }
+        }
+
+
+
     }
 }
